61689d2a22bb98c3aab9e071ef8e2e9a861c700b
noobsiecoder <noobsiecoder@gmail.com>
2023-12-24 17:39:13 -0500

    challenge
    medium: string stack

---
+++ medium/20_valid_parentheses.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+// Runtime: 3ms; Beats 45.18% of users with C++
+// Memory:  6.56MB; Beats 96.91% of users with C++
+class Solution
+{
+public:
+    bool isValid(std::string s)
+    {
+        std::vector<char> stack = {};
+        for (auto chr : s)
+        {
+            if (chr == '(' || chr == '[' || chr == '{')
+            {
+                stack.push_back(chr);
+                continue;
+            }
+
+            if (stack.empty())
+                return false;
+
+            char value = stack[stack.size() - 1];
+            switch (chr)
+            {
+            case ')':
+                if (value != '(')
+                    return false;
+                stack.pop_back();
+                break;
+            case ']':
+                if (value != '[')
+                    return false;
+                stack.pop_back();
+                break;
+            case '}':
+                if (value != '{')
+                    return false;
+                stack.pop_back();
+                break;
+            default:
+                break;
+            }
+        }
+
+        return stack.empty();
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    Solution solution;
+    assert(solution.isValid("(){}[]"));
+    assert(solution.isValid("()}[]"));
+    
+    return 0;
+}
